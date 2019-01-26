8ef5a40a72e995ac8a1850ba99cdb8a40496c1bb
hellotommmy <1728422631@qq.com>
2019-01-26 18:07:36 +0000

    Add files via upload

--- token-bit-4.scala
+++ token-bit-4.scala
@@ -15,8 +15,24 @@ def distinctBy[B, C](xs: List[B], f: B => C, acc: List[C] = Nil): List[B] = xs m
 abstract class Bit
 case object Z extends Bit
 case object S extends Bit
+case class C(c: Char) extends Bit
+
 
 type Bits = List[Bit]
+
+abstract class Action
+case object ST extends Action
+case object NST extends Action
+
+abstract class PartialValue
+case object Plhdr extends PartialValue
+case object STS extends PartialValue
+case object ENDSTS extends PartialValue
+case class Ch(c: Char) extends PartialValue
+case object Empt extends PartialValue
+case object Seque extends PartialValue
+case class Posi(i: Int) extends PartialValue
+case class RECRD(x: String) extends PartialValue
 
 
 abstract class Rexp 
@@ -52,9 +68,8 @@ case class Left(v: Val) extends Val
 case class Right(v: Val) extends Val
 case class Stars(vs: List[Val]) extends Val
 case class Rec(x: String, v: Val) extends Val
-
-
-
+case class Pos(i: Int, v: Val) extends Val
+case object Prd extends Val
    
 // some convenience for typing in regular expressions
 def charlist2rexp(s : List[Char]): Rexp = s match {
@@ -105,41 +120,158 @@ def internalise(r: Rexp) : ARexp = r match {
 }
 
 internalise(("a" | "ab") ~ ("b" | ""))
+val action_stack = scala.collection.mutable.ArrayBuffer.empty[Action]
+val next_stack = scala.collection.mutable.ArrayBuffer.empty[Int]
+val regx_stack = scala.collection.mutable.ArrayBuffer.empty[Rexp]
+val pv_stack = scala.collection.mutable.ArrayBuffer.empty[PartialValue]
+var top = 0
+//st is the global var stack, made with a linked list?
+def decode_stack(sp: Int, bs: Bits): Unit = {
+if(action_stack.isEmpty){
+  return Unit
+}
+val action = action_stack.last
+action_stack.trimEnd(1)
+val r = regx_stack.last
+regx_stack.trimEnd(1)
+if(action == ST)//we have the rest of the star to finish(ST -> STAR)
+{
+  bs match {
+    case Z::bs => {//pv -> partial value  Each grid in a stack does not hold a whole value but a partial one.
 
-
-def decode_aux(r: Rexp, bs: Bits) : (Val, Bits) = (r, bs) match {
-  case (ONE, bs) => (Empty, bs)
-  case (CHAR(c), bs) => (Chr(c), bs)
-  case (ALT(r1, r2), false::bs) => {
-    val (v, bs1) = decode_aux(r1, bs)
-    (Left(v), bs1)
+      pv_stack(sp) = ENDSTS
+      if(next_stack.isEmpty)
+        return Unit
+      val n = next_stack.last
+      next_stack.trimEnd(1)
+      decode_stack(n, bs)
+    }
+    case S::bs => {
+      for(i <- 0 to next_stack.length - 1){
+        next_stack(i) = next_stack(i) + 1
+      }
+      next_stack += (sp + 1)
+      regx_stack += r  
+      action_stack += ST
+      pv_stack.insert(sp + 1, Plhdr)
+      action_stack += NST
+      regx_stack += r 
+      decode_stack(sp, bs)
+    }
   }
-  case (ALT(r1, r2), true::bs) => {
-    val (v, bs1) = decode_aux(r2, bs)
-    (Right(v), bs1)
-  }
-  case (SEQ(r1, r2), bs) => {
-    val (v1, bs1) = decode_aux(r1, bs)
-    val (v2, bs2) = decode_aux(r2, bs1)
-    (Sequ(v1, v2), bs2)
-  }
-  case (STAR(r1), false::bs) => {
-    val (v, bs1) = decode_aux(r1, bs)
-    val (Stars(vs), bs2) = decode_aux(STAR(r1), bs1)
-    (Stars(v::vs), bs2)
-  }
-  case (STAR(_), true::bs) => (Stars(Nil), bs)
-  case (RECD(x, r1), bs) => {
-    val (v, bs1) = decode_aux(r1, bs)
-    (Rec(x, v), bs1)
+}
+else{
+  (r, bs) match{
+    case (ONE, bs) => {
+      pv_stack(sp) = Empt
+      if(next_stack.isEmpty)
+        return Unit
+      val n = next_stack.last
+      next_stack.trimEnd(1)
+      decode_stack(n, bs)
+    }
+    case (PRED(f), C(c)::bs) => {
+      pv_stack(sp) = Ch(c)
+      if(next_stack.isEmpty)
+        return Unit
+      val n = next_stack.last
+      next_stack.trimEnd(1)
+      decode_stack(n, bs)
+    }
+    case (ALTS(rs), bs) => {
+      val le = rs.length
+      val det = bs.take(le - 1)
+      val chosen =  det.indexWhere(_ == Z)
+      action_stack += NST
+      pv_stack.insert(sp + 1, Plhdr)
+      for(i <- 0 to next_stack.length - 1){
+        next_stack(i) = next_stack(i) + 1
+      }
+      if(chosen ==  -1){
+        pv_stack(sp) = Posi(le)
+        regx_stack += rs(le - 1)
+        decode_stack(sp + 1, bs.drop(le - 1))
+      }
+      else{
+        pv_stack(sp) = Posi(chosen + 1)
+        regx_stack += rs(chosen)
+        decode_stack(sp + 1, bs.drop(chosen + 1))
+      }
+    }
+    case (SEQ(r1, r2), bs) => {
+      action_stack += NST
+      action_stack += NST
+      for(i <- 0 to next_stack.length - 1){
+        next_stack(i) = next_stack(i) + 2
+      }
+      next_stack += (sp + 2)
+      regx_stack += r2
+      regx_stack += r1
+      pv_stack.insert(sp + 1, Plhdr)
+      pv_stack.insert(sp + 2, Plhdr)
+      pv_stack(sp) = Seque
+      decode_stack(sp + 1, bs)
+    }
+    case (STAR(r1), S::bs) => {
+      action_stack += ST
+      regx_stack += r1
+      action_stack += NST
+      regx_stack += r1
+      for(i <- 0 to next_stack.length - 1){
+        next_stack(i) = next_stack(i) + 2
+      }
+      next_stack += sp + 2
+      pv_stack(sp) = STS
+      pv_stack.insert(sp + 1, Plhdr)
+      pv_stack.insert(sp + 1, Plhdr)
+      decode_stack(sp + 1, bs)
+    }
+    case (STAR(_), Z::bs) => {
+      pv_stack(sp) = STS 
+      pv_stack.insert(sp + 1, ENDSTS)
+      if(next_stack.isEmpty)
+        return Unit
+      for(i <- 0 to next_stack.length - 1){
+        next_stack(i) = next_stack(i) + 1
+      }
+      val n = next_stack.last
+      next_stack.trimEnd(1)
+      decode_stack(n, bs)
+    }
+    case (RECD(x, r1), bs) => {
+      pv_stack(sp) = RECRD(x)
+      pv_stack.insert(sp + 1, Plhdr)
+      for(i <- 0 to next_stack.length - 1){
+        next_stack(i) = next_stack(i) + 1
+      }
+      action_stack += NST
+      regx_stack += r1
+      decode_stack(sp + 1, bs)
+    }//shouldn't go beyond this point
+    case (_, Nil) => assert(1 == 0)
   }
 }
 
+}
+//advantage: may decode chunks of bits
+def decode(r: Rexp, bs: Bits) = {
+  action_stack.clear()
+  next_stack.clear()
+  regx_stack.clear()
+  pv_stack.clear()
+
+  action_stack += NST
+  regx_stack += r
+  pv_stack += Plhdr
+
+  decode_stack(0, bs)
+}
+/*
 def decode(r: Rexp, bs: Bits) = decode_aux(r, bs) match {
   case (v, Nil) => v
   case _ => throw new Exception("Not decodable")
 }
-
+*/
 
 //erase function: extracts the regx from Aregex
 def erase(r:ARexp): Rexp = r match{
@@ -169,14 +301,14 @@ def mkepsBC(r: ARexp) : Bits = r match {
     bs ++ mkepsBC(rs(n))
   }
   case ASEQ(bs, r1, r2) => bs ++ mkepsBC(r1) ++ mkepsBC(r2)
-  case ASTAR(bs, r) => bs ++ List(S)
+  case ASTAR(bs, r) => bs ++ List(Z)
 }
 
 // derivative of a regular expression w.r.t. a character
 def der(c: Char, r: ARexp) : ARexp = r match {
   case AZERO => AZERO
   case AONE(_) => AZERO
-  case APRED(bs, f) => if (f(c)) AONE(bs) else AZERO
+  case APRED(bs, f) => if (f(c)) AONE(bs:::List(C(c))) else AZERO
   case AALTS(bs, rs) => AALTS(bs, rs.map(der(c, _)))
   case ASEQ(bs, r1, r2) => 
     if (nullable(r1)) AALT(bs, ASEQ(Nil, der(c, r1), r2), fuse(mkepsBC(r1), der(c, r2)))
@@ -253,10 +385,27 @@ def asize(a: ARexp) = size(erase(a))
 
 
 // decoding does not work yet
-//def lexing_simp(r: Rexp, s: String) : Val = decode(r, lex_simp(internalise(r), s.toList))
+def lexing_simp(r: Rexp, s: String) = {
+  val final_derivative = lex_simp(internalise(r), s.toList)
+  println("The length of bit sequence:")
+  println((final_derivative.length))
+  //println(final_derivative)
+  decode(r, final_derivative) 
+  //println(vsize(value))
+}
 
 
-
+def vsize(v: Val): Int = v match {
+  case Empty => 1
+  case Chr(c) => 1
+  case Sequ(v1, v2) => vsize(v1) + vsize(v2) + 1
+  case Left(v1) => vsize(v1) + 1
+  case Right(v1) => vsize(v1) + 1
+  case Stars(vs) => vs.map(vsize(_)).sum + 1
+  case Rec(x, v1) => vsize(v1) + 1
+  case Pos(i, v1) => vsize(v1) + 1
+  case Prd => 1
+}
 
 
 // Lexing Rules for a Small While Language
@@ -312,13 +461,25 @@ val WHILE_REGS = (("k" $ KEYWORD) |
 
 // Some Tests
 //============
-
+def compute_and_print(r: Rexp, s: String){
+  //println(r)
+  //println(s)
+  lexing_simp(r, s)
+  println(pv_stack)
+}
 println("simple tests:")
-println(lex(internalise(SYM.%), "abcd".toList))
-println(lex(internalise((SYM.%) | NUM), "12345".toList))
-println(lex(internalise(WHILE_REGS), "abcd".toList))
-println(lex(internalise(WHILE_REGS), "12345".toList))
-println(lex(internalise(WHILE_REGS), "\nwrite \"Fib\";".toList))
+/*
+println(lexing_simp((SYM.%), "abcd"))
+println(lexing_simp(((SYM.%) | NUM), "12345"))
+println(lexing_simp((WHILE_REGS), "abcd"))
+println(lexing_simp((WHILE_REGS), "12345"))
+println(lexing_simp((WHILE_REGS), "\nwrite \"Fib\";"))
+*/
+compute_and_print((SYM.%), "abcd")
+compute_and_print(((SYM.%) | NUM), "12345")
+compute_and_print((WHILE_REGS), "abcd")
+compute_and_print((WHILE_REGS), "12345")
+compute_and_print((WHILE_REGS), "\nwrite \"Fib\";")
 
 def time[T](code: => T) = {
   val start = System.nanoTime()
@@ -327,9 +488,6 @@ def time[T](code: => T) = {
   println((end - start)/1.0e9)
   result
 }
-
-
-
 
 val prog2 = """
 write "Fib";
@@ -354,8 +512,20 @@ write "Fib";
 */
 
 println("Iteration test with fib")
-for (i <- 0 to 1000 by 10) {
+for (i <- 650 to 1000 by 40) {
   print(i.toString + ":  ")
-  time(lex_simp(internalise(WHILE_REGS), (prog2 * i).toList))
+  time(lexing_simp((WHILE_REGS), (prog2 * i)))
+  //time(lex_simp(internalise(WHILE_REGS), (prog2 * i).toList))
 }
- 
+
+
+/*
+def recurseTest(i:Int):Unit={
+   try{
+      recurseTest(i+1)
+   } catch { case e:java.lang.StackOverflowError => 
+      println("Recursion depth on this system is " + i + ".")
+   }
+}
+recurseTest(0)
+*/
